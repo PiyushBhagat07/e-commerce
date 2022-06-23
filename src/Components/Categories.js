f80de9b723928805679c090834114ab64289c244
Piyush Bhagat <piyushbhagat070@gmail.com>
2022-06-23 23:54:39 +0530

    Updated

---
+++ src/Components/Categories.js
@@ -0,0 +1,110 @@
+const Categories = [
+    {
+        id: 1,
+        title: "Mens black shirt",
+        price: 213,
+        category: "Childern",
+        image: "../../image/1.jpg"
+    },
+    {
+        id: 2,
+        title: "Women black shirt",
+        price: 113,
+        category: "Childern",
+        image: "../../image/2.jpg"
+    },
+    {
+        id: 3,
+        title: "Mens black shirt",
+        price: 213,
+        category: "Childern",
+        image: "../../image/3.jpg"
+    },
+    {
+        id: 4,
+        title: "Women black shirt",
+        price: 113,
+        category: "Childern",
+        image: "../../image/4.jpg"
+    },
+    {
+        id: 5,
+        title: "Mens black shirt",
+        price: 213,
+        category: "Women",
+        image: "../../image/5.jpg"
+    },
+    {
+        id: 6,
+        title: "Women black shirt",
+        price: 113,
+        category: "Women",
+        image: "../../image/6.jpg"
+    },
+    {
+        id: 7,
+        title: "Mens black shirt",
+        price: 213,
+        category: "Women",
+        image: "../../image/7.jpg"
+    },
+    {
+        id: 8,
+        title: "Women black shirt",
+        price: 113,
+        category: "Women",
+        image: "../../image/8.jpg"
+    },
+    {
+        id: 9,
+        title: "Mens black shirt",
+        price: 213,
+        category: "Women",
+        image: "../../image/9.jpg"
+    },
+    {
+        id: 10,
+        title: "Women black shirt",
+        price: 113,
+        category: "Women",
+        image: "../../image/10.jpg"
+    },
+    {
+        id: 11,
+        title: "Mens black shirt",
+        price: 213,
+        category:  "Women",
+        image: "../../image/11.jpg"
+    },
+    {
+        id: 12,
+        title: "Women black shirt",
+        price: 113,
+        category: "Men",
+        image: "../../image/12.jpg"
+    },
+    {
+        id: 13,
+        title: "Mens black shirt",
+        price: 213,
+        category: "Men",
+        image: "../../image/13.jpg"
+    },
+    {
+        id: 14,
+        title: "Women black shirt",
+        price: 113,
+        category:"Men",
+        image: "../../image/14.jpg"
+    },
+    {
+        id: 15,
+        title: "Mens black shirt",
+        price: 213,
+        category: "Men",
+        image: "../../image/15.jpg"
+    },
+   
+]
+
+export default Categories;
